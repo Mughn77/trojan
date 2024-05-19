ab390a7c9143942db6a2ae7b457f6c092fd840c3
Ghani al falah <154010450+Mughn77@users.noreply.github.com>
2024-05-20 02:03:13 +0700

    Update _worker.old.js

--- _worker.old.js
+++ _worker.old.js
@@ -2,7 +2,7 @@
 // src/worker.js
 // https://github.com/ca110us/epeius/blob/main/src/worker.js
 import { connect } from "cloudflare:sockets";
-let sha224Password = 'ghn77';
+let sha224Password = '82912b4d8527de972d787fc294be0c39e6ab42b3295c3176271f4c84';
 let proxyIP = "52.74.101.26";
 
 const worker_default = {
